4af0055fe97b4148a31508ce273a0973f2fc4188
Renato Fraga <rofraga74@gmail.com>
2022-05-27 15:47:54 -0500

    first commit

---
+++ Trivia/Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Renato Oliveira Fraga on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
